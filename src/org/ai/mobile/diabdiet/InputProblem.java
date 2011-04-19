5afd8f817bddf83cd958f60f91600be6e29fa157
EckyPutrady <EckyPutrady@gmail.com>
2011-04-19 17:28:17 +0000

    Initial structure, UI 40%

---
+++ src/org/ai/mobile/diabdiet/InputProblem.java
@@ -0,0 +1,13 @@
+package org.ai.mobile.diabdiet;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class InputProblem extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.input_problem);
+    }
+}

3932cef03eca2b179f11fe71ad2b217460020718
EckyPutrady@gmail.com <EckyPutrady@gmail.com@8abff3a7-1210-3a95-c821-dca47a2f93b5>
2011-04-21 07:41:39 +0000

    Updates dari yogi..

---
+++ trunk/src/org/ai/mobile/diabdiet/SolveProblem.java
@@ -0,0 +1,23 @@
+package org.ai.mobile.diabdiet;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SolveProblem extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+        setContentView(R.layout.solve_problem);
+        
+        //Get say view
+        TextView say = (TextView) findViewById(R.id.solve_say);
+        say.setText("Hello there!");
+        
+        //Invoke solve operation
+        
+        //Display solved problem
+        
+	}
+}

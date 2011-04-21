7630d037aaddb6d49a86d37bb334c93cccd0a0f9
EckyPutrady@gmail.com <EckyPutrady@gmail.com@8abff3a7-1210-3a95-c821-dca47a2f93b5>
2011-04-21 11:22:37 +0000

    Save patient data done..

--- trunk/src/org/ai/data/patient/Anthropometry.java
+++ trunk/src/org/ai/data/patient/Anthropometry.java
@@ -2,9 +2,9 @@ package org.ai.data.patient;
 
 public class Anthropometry {
 	/* all public because this is logically a struct */
-	public boolean isGenderMale;
-	public boolean isPregnant;
-	public int age;
-	public float bodyHeight;
-	public float bodyWeight;
+	public boolean isGenderMale = true;
+	public boolean isPregnant	= false;
+	public int age				= 40;
+	public float bodyHeight		= 170;
+	public float bodyWeight		= 100;
 }
--- trunk/src/org/ai/mobile/diabdiet/InputProblem.java
+++ trunk/src/org/ai/mobile/diabdiet/InputProblem.java
@@ -1,11 +1,20 @@
 package org.ai.mobile.diabdiet;
 
+import java.io.FileNotFoundException;
+
+import org.ai.data.patient.Patient;
+
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class InputProblem extends Activity {
     /** Called when the activity is first created. */
@@ -14,16 +23,93 @@ public class InputProblem extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.input_problem);
         
+        //Load patient
+        loadPatientData();
+        
         //Get button
         Button b = (Button) findViewById(R.id.form_btn_next);
         b.setOnClickListener(new OnClickListener() {			
 			public void onClick(View v) {
 				//Save input to external file
+				updatePatient();
 				
 				//Go to solve screen
 				Intent i = new Intent(InputProblem.this, SolveProblem.class);
 				startActivity(i);
 			}
 		});
+    }
+    
+    protected void updatePatient() {	
+    	//Init patient
+        Patient p = new Patient();
+        
+		//Assign settings
+		p.anthropometry.age = Integer.parseInt(((EditText)findViewById(R.id.in_antro_age)).getText().toString());
+		p.anthropometry.bodyWeight = Float.parseFloat(((EditText)findViewById(R.id.in_antro_bodyheight)).getText().toString());
+		p.anthropometry.bodyWeight = Float.parseFloat(((EditText)findViewById(R.id.in_antro_bodyweight)).getText().toString());
+		p.anthropometry.isGenderMale = ((RadioButton)findViewById(R.id.in_antro_gender_male)).isChecked();
+		p.anthropometry.isPregnant = ((RadioButton)findViewById(R.id.in_antro_preg_yes)).isChecked();
+
+		p.laboratory.cholesterol = Float.parseFloat(((EditText)findViewById(R.id.in_lab_chol)).getText().toString());
+		p.laboratory.diastole = Float.parseFloat(((EditText)findViewById(R.id.in_lab_diastole)).getText().toString());
+		p.laboratory.systole = Float.parseFloat(((EditText)findViewById(R.id.in_lab_systole)).getText().toString());
+		p.laboratory.HDL = Float.parseFloat(((EditText)findViewById(R.id.in_lab_HDL)).getText().toString());
+		p.laboratory.LDL = Float.parseFloat(((EditText)findViewById(R.id.in_lab_LDL)).getText().toString());
+		p.laboratory.temperature = Float.parseFloat(((EditText)findViewById(R.id.in_lab_temp)).getText().toString());
+		p.laboratory.triglyceride = Float.parseFloat(((EditText)findViewById(R.id.in_lab_trig)).getText().toString());
+		p.anthropometry.bodyWeight = Float.parseFloat(((EditText)findViewById(R.id.in_lab_uricacid)).getText().toString());
+
+		p.clinicalAnamnesa.swallowProb = ((CheckBox)findViewById(R.id.in_anam_swallow)).isChecked();
+		p.clinicalAnamnesa.bloodVeinProb = ((CheckBox)findViewById(R.id.in_clinic_bloodvein)).isChecked();
+		p.clinicalAnamnesa.cellulitis = ((CheckBox)findViewById(R.id.in_clinic_cellulitis)).isChecked();
+		p.clinicalAnamnesa.diabMoreThan2Years = ((CheckBox)findViewById(R.id.in_clinic_diablong)).isChecked();
+		p.clinicalAnamnesa.fracture = ((CheckBox)findViewById(R.id.in_clinic_fracture)).isChecked();
+		p.clinicalAnamnesa.hungerControl = ((CheckBox)findViewById(R.id.in_clinic_standhunger)).isChecked();
+		p.clinicalAnamnesa.wound = ((CheckBox)findViewById(R.id.in_clinic_wound)).isChecked();
+		
+		//Save patient
+		try {
+			p.save(openFileOutput(FilePaths.PATIENT, Context.MODE_PRIVATE));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	private void loadPatientData() {    
+        //Load file
+        Patient p = new Patient();
+        try {
+			p.load(openFileInput(FilePaths.PATIENT));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+        
+        //Assign
+		((EditText)findViewById(R.id.in_antro_age)).setText("" + p.anthropometry.age);
+		((EditText)findViewById(R.id.in_antro_bodyheight)).setText("" + p.anthropometry.bodyHeight);
+		((EditText)findViewById(R.id.in_antro_bodyweight)).setText("" + p.anthropometry.bodyWeight);
+		if(p.anthropometry.isGenderMale) ((RadioButton)findViewById(R.id.in_antro_gender_male)).toggle();
+		else ((RadioButton)findViewById(R.id.in_antro_gender_female)).toggle();
+		if(p.anthropometry.isPregnant) ((RadioButton)findViewById(R.id.in_antro_preg_yes)).toggle();
+		else ((RadioButton)findViewById(R.id.in_antro_preg_no)).toggle();
+
+		((EditText)findViewById(R.id.in_lab_chol)).setText("" + p.laboratory.cholesterol);
+		((EditText)findViewById(R.id.in_lab_diastole)).setText("" + p.laboratory.diastole);
+		((EditText)findViewById(R.id.in_lab_systole)).setText("" + p.laboratory.systole);
+		((EditText)findViewById(R.id.in_lab_HDL)).setText("" + p.laboratory.HDL);
+		((EditText)findViewById(R.id.in_lab_LDL)).setText("" + p.laboratory.LDL);
+		((EditText)findViewById(R.id.in_lab_temp)).setText("" + p.laboratory.temperature);
+		((EditText)findViewById(R.id.in_lab_trig)).setText("" + p.laboratory.triglyceride);
+		((EditText)findViewById(R.id.in_lab_uricacid)).setText("" + p.anthropometry.bodyWeight);
+
+		((CheckBox)findViewById(R.id.in_anam_swallow)).setChecked(p.clinicalAnamnesa.swallowProb);
+		((CheckBox)findViewById(R.id.in_clinic_bloodvein)).setChecked(p.clinicalAnamnesa.bloodVeinProb);
+		((CheckBox)findViewById(R.id.in_clinic_cellulitis)).setChecked(p.clinicalAnamnesa.cellulitis);
+		((CheckBox)findViewById(R.id.in_clinic_diablong)).setChecked(p.clinicalAnamnesa.diabMoreThan2Years);
+		((CheckBox)findViewById(R.id.in_clinic_fracture)).setChecked(p.clinicalAnamnesa.fracture);
+		((CheckBox)findViewById(R.id.in_clinic_standhunger)).setChecked(p.clinicalAnamnesa.hungerControl);
+		((CheckBox)findViewById(R.id.in_clinic_wound)).setChecked(p.clinicalAnamnesa.wound);
     }
 }

bbb60f6d1ceebe7846d2bf17fe4c2ec14490297b
EckyPutrady@gmail.com <EckyPutrady@gmail.com@8abff3a7-1210-3a95-c821-dca47a2f93b5>
2011-04-27 06:31:03 +0000

    layouting di editdietmenu.xml dim

---
+++ trunk/src/org/ai/diabdiet/mobile/EditDietMenu.java
@@ -0,0 +1,15 @@
+package org.ai.diabdiet.mobile;
+
+import org.ai.mobile.diabdiet.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EditDietMenu extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+        setContentView(R.layout.editdietmenu);
+	}
+}
